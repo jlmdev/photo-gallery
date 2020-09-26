d08a20b8a19e9ece0c2cd12f3cd0b378f7d096d7
Josh Mann <jlmann72@gmail.com>
2020-09-26 06:23:00 -0400

    Working on formatting detail pages.

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -11,46 +11,44 @@ export class CategoryList extends Component {
       <div>
         <>
           <body className="hero-image">
-            <div>
-              <header>
-                <h1>A few of my favorite things...</h1>
-                <nav>
-                  <ul>
-                    <li>
-                      <Link to="/">Go Home</Link>
-                    </li>
-                    <li>
-                      <Link to="/pandas">Pandas</Link>
-                    </li>
-                    <li>
-                      <Link to="/miniatures">Miniatures</Link>
-                    </li>
-                  </ul>
-                </nav>
-                <span className="photo-credit">
-                  Background Photo by{' '}
-                  <a href="https://unsplash.com/@artplay?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-                    Maxim Selyuk
-                  </a>{' '}
-                  on{' '}
-                  <a href="https://unsplash.com/s/photos/tools?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-                    Unsplash
-                  </a>
-                </span>
-              </header>
-              <Switch>
-                <Route exact path="/"></Route>
-                <Route exact path="/pandas" component={Pandas} />
-                <Route path="/pandas/:index" component={PandaPhotoDetail} />
-                <Route exact path="/miniatures" component={Miniatures} />
-                <Route
-                  path="/miniatures/:index"
-                  component={MiniaturePhotoDetail}
-                />
-                <Route path="*">Not Found</Route>
-              </Switch>
-              <p></p>
-            </div>
+            <header>
+              <h1>A few of my favorite things...</h1>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Go Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/pandas">Pandas</Link>
+                  </li>
+                  <li>
+                    <Link to="/miniatures">Miniatures</Link>
+                  </li>
+                </ul>
+              </nav>
+              {/* <span className="photo-credit">
+                Background Photo by{' '}
+                <a href="https://unsplash.com/@artplay?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                  Maxim Selyuk
+                </a>{' '}
+                on{' '}
+                <a href="https://unsplash.com/s/photos/tools?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                  Unsplash
+                </a>
+              </span> */}
+            </header>
+            <Switch>
+              <Route exact path="/"></Route>
+              <Route exact path="/pandas" component={Pandas} />
+              <Route path="/pandas/:index" component={PandaPhotoDetail} />
+              <Route exact path="/miniatures" component={Miniatures} />
+              <Route
+                path="/miniatures/:index"
+                component={MiniaturePhotoDetail}
+              />
+              <Route path="*">Not Found</Route>
+            </Switch>
+            <p></p>
           </body>
         </>
       </div>
--- src/components/Pandas.jsx
+++ src/components/Pandas.jsx
@@ -11,20 +11,22 @@ export class Pandas extends Component {
 
   render() {
     return (
-      <div>
+      <div class="photo-entry">
         <h2>{this.state.pandas.title}</h2>
         <p>{this.state.pandas.description}</p>
-        {this.state.photos.map((photo, index) => (
-          <figure>
-            <Link to={`/pandas/${index}`}>
-              <img src={photo.imageURL} alt={photo.title} />
-            </Link>
-            <figcaption>
-              {photo.title}
-              <a href={photo.sourceURL}>Source</a>
-            </figcaption>
-          </figure>
-        ))}
+        <section className="photo-list">
+          {this.state.photos.map((photo, index) => (
+            <figure>
+              <Link to={`/pandas/${index}`} className="image-link">
+                <img src={photo.imageURL} alt={photo.title} />
+              </Link>
+              <figcaption>
+                {photo.title}
+                <a href={photo.sourceURL}>Source</a>
+              </figcaption>
+            </figure>
+          ))}
+        </section>
       </div>
     )
   }
